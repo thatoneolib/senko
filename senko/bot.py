be5d5b2b7902620be714f9a68eab5db97ca82956
Maxee <maxee.dev@gmail.com>
2020-10-28 16:43:17 +0100

    Add more default arguments for bot class

--- senko/bot.py
+++ senko/bot.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import discord
 from discord.ext import commands
 
 from .i18n import Locales
@@ -14,14 +15,10 @@ class Senko(commands.AutoShardedBot):
 
     Parameters
     ----------
-    \*args
-        Positional arguments to pass on to the parent class.
     db: asyncpg.pool.Pool
         An asyncpg database connection pool.
     session: aiohttp.ClientSession
         An aiohttp client session.
-    \*\*kwargs
-        Keyword arguments to pass on to the parent class.
 
     Attributes
     ----------
@@ -32,28 +29,34 @@ class Senko(commands.AutoShardedBot):
     locales: senko.Locales
         A pool of loaded locales.
     """
-    def __init__(self, *args, db=None, session=None, **kwargs):
-        # Default parameters
-        command_prefix = kwargs.pop("command_prefix", None)
-        if command_prefix is None:
-            command_prefix = commands.when_mentioned_or(self.config.prefix)
-
-        case_insensitive = kwargs.pop("case_insensitive", None)
-        if case_insensitive is None:
-            case_insensitive = True
+    def __init__(self, db, session):
+        
+        # Prepare and call the parent constructor.
+        intents = discord.Intents.all()
+        intents.presences = False # Requires verification.
+        intents.integrations = False
+        intents.webhooks = False
+        intents.invites = False
+        intents.voice_states = False
+        intents.typing = False
+        
+        member_cache_flags = discord.MemberCacheFlags.from_intents(intents)
+        command_prefix = commands.when_mentioned_or(self.config.prefix)
 
         super().__init__(
-            *args,
+            intents=intents,
+            member_cache_flags=member_cache_flags,
             command_prefix=command_prefix,
-            case_insensitive=case_insensitive,
-            **kwargs,
+            case_insensitive=True,
+            chunk_guilds_at_startup=False,
+            guild_subscriptions=False
         )
 
-        # Attributes
+        # Set general attributes.
         self.log = logging.getLogger("senko.bot")
         self.db = db
         self.session = session
-
+        
         self._exit_code = 0
 
         # Interfaces
